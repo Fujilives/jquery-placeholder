5d3d1c81229583caef7a5470b29c6be2489f8084
Fujilives <Fujilives@yahoo.com>
2013-03-12 22:36:24 -0500

    Update jquery.placeholder.js
    Updated to use Div containers.
    Way divs are sized changed.
    
    Seems to line up really well in all my tests now, and allows you to absolutely position divs.
    
    Only problem I cannot seem to resolve is if you have an absolutely positioned input using %, it won't update position on window resize.  If someone can enable that I think we're pretty golden.

--- jquery.placeholder.js
+++ jquery.placeholder.js
@@ -13,17 +13,27 @@
  *
  * @author Brian Reavis <brian@thirdroute.com>
  */
-
 (function($) {
 
 	var NATIVE_SUPPORT = ('placeholder' in document.createElement('input'));
-	var CSS_PROPERTIES = [
+	var CONTAINER_CSS_PROPERTIES = [
+		'-moz-box-sizing', '-webkit-box-sizing', 'box-sizing',
+		'padding-top', 'padding-right', 'padding-bottom', 'padding-left',
+		'margin-top', 'margin-right', 'margin-bottom', 'margin-left',
+		'border-top-width', 'border-right-width', 'border-bottom-width', 'border-left-width',
+		'line-height', 'font-size', 'font-family',
+		'float', 'text-align', 'position', 'width', 'height',
+		'top', 'left', 'right', 'bottom'
+	];
+	
+	var PLACEHOLDER_CSS_PROPERTIES = [
 		'-moz-box-sizing', '-webkit-box-sizing', 'box-sizing',
 		'padding-top', 'padding-right', 'padding-bottom', 'padding-left',
 		'margin-top', 'margin-right', 'margin-bottom', 'margin-left',
 		'border-top-width', 'border-right-width', 'border-bottom-width', 'border-left-width',
 		'line-height', 'font-size', 'font-family', 'width', 'height',
-		'top', 'left', 'right', 'bottom'
+		'text-align',
+		//'top', 'left', 'right', 'bottom'
 	];
 
 	var setupPlaceholder = function(input, options) {
@@ -41,17 +51,62 @@
 			return;
 		}
 
-		// enumerate textbox styles for mimicking
-		styles = {};
-		for (i = 0; i < CSS_PROPERTIES.length; i++) {
-			styles[CSS_PROPERTIES[i]] = $input.css(CSS_PROPERTIES[i]);
-		}
+		// enumerate textbox styles for mimicking - these will be applied to the container div
+		container_styles = {};
+		for (i = 0; i < CONTAINER_CSS_PROPERTIES.length; i++) {
+			container_styles[CONTAINER_CSS_PROPERTIES[i]] = $input.css(CONTAINER_CSS_PROPERTIES[i]);
+		};
+
+		// enumerate textbox styles for mimicking - these will be applied to the placeholder overlay
+		placeholder_styles = {};
+		for (i = 0; i < PLACEHOLDER_CSS_PROPERTIES.length; i++) {
+			placeholder_styles[PLACEHOLDER_CSS_PROPERTIES[i]] = $input.css(PLACEHOLDER_CSS_PROPERTIES[i]);
+		};
+		
+		
 		zIndex = parseInt($input.css('z-index'), 10);
 		if (isNaN(zIndex) || !zIndex) zIndex = 1;
+		
+	
+		// create the placeholder container div - needs to be created before PH overlay span
+		$container = $('<div>').addClass('placeholder-container');
+		$container.css(container_styles);
+		$container.css({
+			'cursor': $input.css('cursor') || 'text',
+			'display': 'block',
+			//'position': 'relative',
+			'overflow': 'hidden',
+			'z-index': zIndex + 1,
+			'background': 'none',
+			'border-top-style': 'solid',
+			'border-right-style': 'solid',
+			'border-bottom-style': 'solid',
+			'border-left-style': 'solid',
+			'border-top-color': 'transparent',
+			'border-right-color': 'transparent',
+			'border-bottom-color': 'transparent',
+			'border-left-color': 'transparent',
+			'border-top-width': '0',
+			'border-right-width': '0',
+			'border-bottom-width': '0',
+			'border-left-width': '0',
+			'padding-right': '0',
+			'padding-left': '0',
+			'padding-top': '0',
+			'padding-bottom': '0',
+			'margin-right': 'auto',
+			'margin-left': 'auto',
+			'margin-top': 'auto',
+			'margin-bottom': 'auto',
+			'width': ( parseInt($container.css("width")) + parseInt($container.css("padding-left")) + parseInt($container.css("padding-right")) + parseInt($(input).css("border-left-width")) + parseInt($(input).css("border-right-width")) ) + 'px',
+			'height': ( parseInt($container.css("height")) + parseInt($container.css("padding-top")) + parseInt($container.css("padding-bottom")) + parseInt($(input).css("border-top-width")) + parseInt($(input).css("border-bottom-width")) ) + 'px',
+		});
+		$(input).wrap($container);
+		
 
-		// create the placeholder
+		// create the placeholder overlay span - now created inside the container div generated above
 		$placeholder = $('<span>').addClass('placeholder').html(text);
-		$placeholder.css(styles);
+		$placeholder.css(placeholder_styles);
 		$placeholder.css({
 			'cursor': $input.css('cursor') || 'text',
 			'display': 'block',
@@ -66,10 +121,22 @@
 			'border-top-color': 'transparent',
 			'border-right-color': 'transparent',
 			'border-bottom-color': 'transparent',
-			'border-left-color': 'transparent'
+			'border-left-color': 'transparent',
+			'line-height': $(input).css("height"),
+			'left': '50%',
+			'margin-left': (parseInt($placeholder.css("width")) / -2) + (parseInt($placeholder.css("padding-left")) * -1) - parseInt($(input).css("border-left-width")) + 'px'
 		});
 		$placeholder.insertBefore($input);
-
+		
+		
+		$(input).css({
+			'top': 'auto',
+			'right': 'auto',
+			'bottom': 'auto',
+			'left': 'auto'
+		});
+		
+		
 		// compensate for y difference caused by absolute / relative difference (line-height factor)
 		dy = $input.offset().top - $placeholder.offset().top;
 		marginTop = parseInt($placeholder.css('margin-top'));
